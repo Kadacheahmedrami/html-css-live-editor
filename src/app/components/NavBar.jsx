48651acf77a5fe0a3ebbba722ab194630a15ce51
Kadacheahmedrami <a_kadache@estin.dz>
2024-12-10 14:47:44 +0100

    ui better

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,14 +1,25 @@
 import Image from 'next/image'
 import React from 'react'
+import localFont from "next/font/local";
+
+
+const minecrafterAlt = localFont({
+  src:'../fonts/Minecrafter.Alt.ttf',
+  display: 'swap',
+  weight: '300', // Specify weight if needed
+  style: 'normal' // Specify style if needed
+});
+
 
 function NavBar() {
   return (
     <nav className="navbar">
-    <div className="logo" draggable="false">
-      <a href='/wlcm' className='flex justify-center items-center gap-2'>
+    <div className="logo " draggable="false">
+      <a href='/' className='flex justify-center items-center gap-2'>
       <Image src={"/sir.svg"} className="" height={60} width={60} alt="logo" />
+      <h1 className={`text-[40px] text-[#754DD2] ${minecrafterAlt.className}   font-semibold my-auto`}>Sirius CSS</h1>
       </a>
-   
+     
     </div>
     <ul className="nav-links">
     <li><a className='linea'  href="#Transform">Transform</a></li>
